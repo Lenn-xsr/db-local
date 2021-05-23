f719f652b35ac04dd0c7446ce96082a7eae125e6
Lennart <55673351+Lenn-xsr@users.noreply.github.com>
2021-05-23 19:30:14 -0300

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,7 +1,13 @@
-const { User } = require("./schemas.js");
+const dbLocal = require("../lib");
+const { Schema } = new dbLocal({ path: "./databases", pack: true });
+
+const User = Schema("User", {
+  _id: { type: Number },
+  username: { type: String, required: true },
+  bots: { type: Array, default: [{ name: "test" }] },
+});
 
 const user = User.find({ _id: 2 });
-
-if (!user) User.create({ _id: 2, username: "teste" }).save();
+if (!user) User.create({ _id: 2, username: "test" }).save();
 
 console.log(user);
