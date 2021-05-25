fa1e5e842e348a3411c6446c2448e0a205e28c56
Lennart-sr <lennart.xsr@gmail.com>
2021-05-24 21:15:37 -0300

    Adjusting methods

--- example/index.js
+++ example/index.js
@@ -1,13 +1,12 @@
-const dbLocal = require("db-local");
-const { Schema } = new dbLocal({ path: "./databases", compress: true });
+const dbLocal = require("../lib/index.js");
+const { Schema } = new dbLocal({ path: "./databases", compress: false });
 
 const User = Schema("User", {
   _id: { type: Number },
   username: { type: String, required: true },
-  bots: { type: Array, default: [{ name: "test" }] },
 });
 
 let user = User.find({ _id: 1 });
-if (!user) User.create({ _id: 1, username: "test" }).save();
+if (!user) user = User.create({ _id: 1, username: "lennart" }).save();
 
 console.log(user);
